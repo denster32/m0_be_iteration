bfb0128f1168d5989c48ca7ce95d3eaf222871f1
Dennis Palucki <paluckide@yahoo.com>
2022-06-10 06:25:35 -0500

    Make syntax changes

--- each.rb
+++ each.rb
@@ -4,18 +4,17 @@
 
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
-weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+weather_option = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
-weather_options.each do |weather_options|
-  puts "Today the weather is #{weather_options}"
+weather_option.each do |weather_option|
+  puts "Today the weather is #{weather_option}"
 end
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
-words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+word = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
-words.each do |words|
-  if words.length < 4
-    puts " #{words}"
+word.each do |word|
+  if word.length == 3
+    puts word
   end
 end
-# Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
